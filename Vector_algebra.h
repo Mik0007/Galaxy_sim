fe8b7941589bdf72441ba8e17c91b642c69b82b2
Mik0007 <69309488+Mik0007@users.noreply.github.com>
2023-03-29 22:02:47 +0200

    Update Vector_algebra.h

--- Vector_algebra.h
+++ Vector_algebra.h
@@ -6,20 +6,16 @@ using namespace std;
 
 class vec {
 	public:
-		static float* molt_scal(float t, float* vec, int size = 3)//t*vec
+		static void molt_scal(float* res, float t, float* vec, int size = 3)//t*vec
 		{
-			float* v = new float[3];
 			for (int i = 0; i < size; i++)
-				v[i] = t*vec[i]; 
-			return v;
+				res[i] = t*vec[i]; 
 		}
 		
-		static float* linear_sum(float t, float* vec, float* vec0, int size = 3)//vec0 + t*vec
+		static void linear_sum(float* res, float t, float* vec, float* vec0, int size = 3)//vec0 + t*vec
 		{
-			float* v = new float[3];
 			for (int i = 0; i < size; i++)
-				v[i] = t*vec[i] + vec0[i]; 
-			return v;
+				res[i] = t*vec[i] + vec0[i];
 		}
 		
 		static float squared(float* vec, int size = 3)//vec^2
@@ -30,28 +26,22 @@ class vec {
 			return mod2;
 		}
 		
-		static float* sum(float* vec1, float* vec2, int size = 3)//vec1 + vec2
+		static void sum(float* res, float* vec1, float* vec2, int size = 3)//vec1 + vec2
 		{
-			float* v = new float[3];
 			for (int i = 0; i < size; i++)
-				v[i] = vec1[i] + vec2[i]; 
-			return v;
+				res[i] = vec1[i] + vec2[i];
 		}
 		
-		static float* sum(float* vec1, float* vec2, float* vec3, int size = 3)//vec1 + vec2
+		static void sum(float* res, float* vec1, float* vec2, float* vec3, int size = 3)//vec1 + vec2
 		{
-			float* v = new float[3];
 			for (int i = 0; i < size; i++)
-				v[i] = vec1[i] + vec2[i] + vec3[i]; 
-			return v;
+				res[i] = vec1[i] + vec2[i] + vec3[i]; 
 		}
 		
-		static float* d(float* vec1, float* vec2, int size = 3)//distanza tra vettori
+		static void d(float* res, float* vec1, float* vec2, int size = 3)//distanza tra vettori
 		{
-			float* v = new float[3];
 			for (int i = 0; i < size; i++)
-				v[i] = vec1[i] - vec2[i]; 
-			return v;
+				res[i] = vec1[i] - vec2[i];
 		}
 		
 		static void azzera(float* vec, int size = 3)
@@ -60,32 +50,10 @@ class vec {
 				vec[i] =  0;
 		}
 		
-		static void copy(float* vec, float* vec0, int size = 3)
+		static void copy(float* res, float* vec0, int size = 3)
 		{
 			for (int i = 0; i < size; i++)
-				vec[i] = vec0[i];
-		}
-		
-		static float angle(float* vec1, float* vec2, int size = 3)
-		{
-			float P = 0;
-			for (int i = 0; i < size; i++)
-				P += vec1[i]*vec2[i];
-			P /= sqrt( squared(vec1) );
-			P /= sqrt( squared(vec2) );
-			return acos(P);
-		}
-		
-		static float cos2_angle(float* vec1, float* vec2, int size = 3)
-		{
-			//per capire vedi f precedente
-			float P = 0;
-			for (int i = 0; i < size; i++)
-				P += vec1[i]*vec2[i];
-			P = P*P;
-			P /= squared(vec1);
-			P /= squared(vec2);
-			return P;
+				res[i] = vec0[i];
 		}
 		
 		static void rotate(float* vec, float angle)//2D
@@ -98,23 +66,6 @@ class vec {
 			vec[0] = x * c - y * s;
 			vec[1] = x * s + y * c;
 		}
-		
-//		static void rotate(float* vec, float theta, float phi)//3D
-//		{
-//			float x = vec[0];
-//			float y = vec[1];
-//			float z = vec[2]:
-//				
-//			float ct = cos(theta);
-//			float st = sin(theta);
-//			float cp = cos(phi);
-//			float sp = sin(phi);
-
-//			da finire
-
-//			vec[0] = x * c - y * s;
-//			vec[1] = x * s + y * c;
-//		}
 		
 	private:
 		vec(){}
